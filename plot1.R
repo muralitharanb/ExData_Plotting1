a54be1127e2b09581324bc8c674d5ce2ebd7ca1a
muralitharanb@gmail.com <muralitharanb@gmail.com>
2018-09-22 17:18:18 +0530

    color added in plot1

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ plot1 <- function(){
     few_day_data_set <- subset(full_data_set, Date == "1/2/2007" | Date == "2/2/2007" )
     hist(as.numeric(few_day_data_set$Global_active_power),freq=TRUE,
          main = "Global Active Power",
-         xlab="Global Active Power(kilowatts)",ylab="Frequency"
+         xlab="Global Active Power(kilowatts)",ylab="Frequency",col = "red"
          )
     dev.copy(png, file ="plot1.png")
     dev.off()
